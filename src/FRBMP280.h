38a575000294e931796374fa8a306b2bf581a6ce
josmeuleman <josmeuleman@gmail.com>
2024-03-15 16:45:37 +0100

    Update FRBMP280.h

--- src/FRBMP280.h
+++ src/FRBMP280.h
@@ -19,7 +19,8 @@ class FRBMP280 : public FRSensor {
 
     private:
         Adafruit_BMP280* _myBMP;
-        static const int MY_BMP280_ADDRESS = 0x76;
+        static const int BMP280_ADDRESS = 0x76;
+		float _offetPressure;
 };
 
 #endif
