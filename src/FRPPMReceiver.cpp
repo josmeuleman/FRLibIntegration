9db472628df9f82beaf1c6e7125097991fe33372
josmeuleman <josmeuleman@gmail.com>
2024-03-19 23:57:57 +0100

    Update FRPPMReceiver.cpp

--- src/FRPPMReceiver.cpp
+++ src/FRPPMReceiver.cpp
@@ -12,7 +12,7 @@ volatile unsigned long FRPPMReceiver::_lastPulseUS;
 volatile uint16_t FRPPMReceiver::_channel;
 volatile int* FRPPMReceiver::_channelValues;
 
-FRPPMReceiver::FRPPMReceiver(int pinNumber, int numberOfChannels) {
+FRPPMReceiver::FRPPMReceiver(byte pinNumber, byte numberOfChannels) {
   _instance = this; // Store a pointer to this instance
   _numberOfChannels = numberOfChannels;
   _pinNumber = pinNumber;
@@ -22,7 +22,7 @@ FRPPMReceiver::FRPPMReceiver(int pinNumber, int numberOfChannels) {
   }
   _channelValues = new int[_numberOfChannels];
   
-  for (int i = 0; i < _numberOfChannels; i++) {
+  for (byte i = 0; i < _numberOfChannels; i++) {
 	_channelValues[i] = 0;
   }
   _channel = 0;
@@ -44,7 +44,7 @@ void FRPPMReceiver::SetPrefix(String prefix) {
   _prefix = prefix; 
 }
 
-int FRPPMReceiver::ReadChannel(int ChannelNumber) {
+int FRPPMReceiver::ReadChannel(byte ChannelNumber) {
   if ((ChannelNumber >= 0) && (ChannelNumber < _numberOfChannels)) {
     return (_channelValues[ChannelNumber]);
   } 
@@ -53,9 +53,24 @@ int FRPPMReceiver::ReadChannel(int ChannelNumber) {
   }
 }
 
+bool FRPPMReceiver::IsChannelHigh(byte ChannelNumber){
+	return (_channelValues[ChannelNumber] < PPMTHRESHMID);
+}
+
+triStateSwitch FRPPMReceiver::GetChannelTriState(byte ChannelNumber){
+  if (_channelValues[ChannelNumber] < PPMTHRESHLOW) {
+    return HISTATE;
+  } else if (_channelValues[ChannelNumber] < PPMTHRESHHIGH) {
+    return MIDSTATE;
+  } else {
+    return LOSTATE;
+  }
+}
+
+
 String FRPPMReceiver::HeaderString(){
   String tempString;
-  for (int i = 0; i < _numberOfChannels; i++) {
+  for (byte i = 0; i < _numberOfChannels; i++) {
 	tempString.concat(_prefix);
 	tempString.concat(i);
 	tempString.concat("; ");
@@ -65,7 +80,7 @@ String FRPPMReceiver::HeaderString(){
 
 String FRPPMReceiver::SensorString(){
   String tempString;
-  for (int i = 0; i < _numberOfChannels; i++) {
+  for (byte i = 0; i < _numberOfChannels; i++) {
 	tempString.concat(createFloatString(_channelValues[i], 2));  
   }
   return tempString;
