93832502f6b1145595d3b4e0e82cdc626e02edcb
josmeuleman <josmeuleman@gmail.com>
2024-03-21 22:51:15 +0100

    Update FRPPMReceiver.h

--- src/FRPPMReceiver.h
+++ src/FRPPMReceiver.h
@@ -15,41 +15,41 @@ const int PPMTHRESHHIGH = 1350;
 
 // Some switches have three states. We make constants defining LOSTATE (-1), MIDSTATE (0) and HISTATE(1)
 typedef enum triStateSwitch {
-  LOSTATE = -1,
-  MIDSTATE,
-  HISTATE
+	LOSTATE = -1,
+	MIDSTATE,
+	HISTATE
 };
 
 class FRPPMReceiver : public FRSensor{
-  public:
-  FRPPMReceiver(byte pinNumber, byte numberOfChannels);
-  ~FRPPMReceiver();
-  
-  void Init();
-  void SetPrefix(String prefix);
-  
-  int ReadChannel(byte ChannelNumber);
-  bool IsChannelHigh(byte ChannelNumber);
-  triStateSwitch GetChannelTriState(byte ChannelNumber);
-  
-  String HeaderString() override;
-  String SensorString() override;
-  
-  private:
-  byte _pinNumber;
-  byte _numberOfChannels;
-  static volatile unsigned long _lastPulseUS;
-  static volatile uint16_t _channel;
-  static volatile int* _channelValues;
-  
-  static FRPPMReceiver* _instance;
-  const int _MAXPULSEWIDTH = 1800;
-  const int _MINPULSEWIDTH = 400;
-  const int _PULSETHRESHUS = 3000; // Threshold for pulse duration in microseconds
-  String _prefix = "Ch";  
-  
-  void CountPulse();
-  static void ppm_isr(void* arg);  
+	public:
+		FRPPMReceiver(byte pinNumber, byte numberOfChannels);
+		~FRPPMReceiver();
+
+		void Init();
+		void SetPrefix(String prefix);
+
+		int ReadChannel(byte ChannelNumber);
+		bool IsChannelHigh(byte ChannelNumber);
+		triStateSwitch GetChannelTriState(byte ChannelNumber);
+
+		String HeaderString() override;
+		String SensorString() override;
+
+	private:
+		byte _pinNumber;
+		byte _numberOfChannels;
+		static volatile unsigned long _lastPulseUS;
+		static volatile uint16_t _channel;
+		static volatile int* _channelValues;
+
+		static FRPPMReceiver* _instance;
+		const int _MAXPULSEWIDTH = 1800;
+		const int _MINPULSEWIDTH = 400;
+		const int _PULSETHRESHUS = 3000; // Threshold for pulse duration in microseconds
+		String _prefix = "Ch";  
+
+		void CountPulse();
+		static void ppm_isr(void* arg);  
   
 
 };
