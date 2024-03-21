6fe37d67c782f21944ebc5ea0a7ab1c22fb0a4c6
josmeuleman <josmeuleman@gmail.com>
2024-03-21 16:33:00 +0100

    Update FRServoControlTemplate.ino

--- examples/FRServoControlTemplate/FRServoControlTemplate.ino
+++ examples/FRServoControlTemplate/FRServoControlTemplate.ino
@@ -3,7 +3,8 @@
 // - 1x Flight Recorder Board with ESP32
 // - 1x Turnigy TGY IA6B Receiver programmed in PPL mode
 // - 1x Tunigy TGY-i6S Controller programmed in PPL mode
-// - 2x Servo motors 
+// - 2x Servo motors
+// - 1x RGB LED 
 // Connections:
 // - TGY channel PPM, pin 1 - FR Board, PPL2 GND
 // - TGY channel PPM, pin 2 - FR Board, PPL2, D4
@@ -13,52 +14,43 @@
 // - FRLibIntegration (download from https://github.com/josmeuleman/FRLibIntegration, unzipped in ../Documents/Arduino/libraries/ )
 // - ESP32Servo.h
 //
-// 2023-04-20, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
+// 2024-03-21, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
 
-#include <ESP32Servo.h>
+// Libraries from FRLibBasics
 #include <FRTimer.h>
 #include <FRButton.h>
+#include <FRRGBLED.h>
+
+// Libraries from FRLibIntegration
 #include <FRPPMReceiver.h>
 
-const byte NUMBEROFSERVOS = 4;       // Number of servos 
+// Other libraries
+#include <ESP32Servo.h>
+
+
+const byte NUMBEROFSERVOS = 2;       // Number of servos 
 const byte NUMBEROFCHANNELS = 8;     // Number of Channels of myReceiver
 
+// Pins
 const byte PINSWITCH = 35;           // The pin number for he button to start and stop logging
-const byte PINLED = 12;              // The pin number for the LED
-const byte PINSERVO[NUMBEROFSERVOS] = {25, 26, 27, 14}; // Servo Channels
+const byte PINSERVO[NUMBEROFSERVOS] = {25, 26}; // Servo Channels
 const byte PINPPM = 4;               // PM2 input pint
 
-const int  LOOPTIMELOGGERMS = 100;   // Loop time for reading the AD channel in milliseconds
-const int  LOOPTIMESERVOMS = 100;     // Loop time for reading the AD channel in milliseconds
+// other constants
+const int  LOOPTIMESERVOMS = 100;     // Loop time for controlling servos
 
 
 // Create all objects
-Timer loggerTimer(LOOPTIMELOGGERMS);  // Timer object for the clock
-Timer servoTimer(LOOPTIMESERVOMS);    // Timer object for the clock
-
-Logger myLogger;                    // Logger object for logging sensors to the SD
-Button myButton(PINSWITCH, true);   // Create a button object with the given pin. True for an inverted button, false for a normal button
-LED myLed(PINLED);                  // Create a led object with the given pin.
+Timer servoTimer(LOOPTIMESERVOMS);  // Timer object for the clock
 Servo myServo[NUMBEROFSERVOS];      // create a servo object
-
+Button myButton(PINSWITCH, true);   // Create a button object with the given pin. True for an inverted button, false for a normal button
+RGBLED myLed;                       // Create a RGB led object. pinnummbers are defined in the library FRRGBLED.h.
 FRPPMReceiver myReceiver(PINPPM, NUMBEROFCHANNELS);  // Create a PPM receiver object with given pin and number of channels
+
 int channelValues[NUMBEROFCHANNELS];
-// Suggestion for signal mapping
-// channel0 - tick up --> handle cargo drop servo
-// channel1 - NA
-// channel2 - NA
-// channel3 - NA
-// channel4 - SWA UP/DOWN -> stop/start logging
-// channel5 - SWB UP/MID/DOWN -> gears down & hatch open / gears up & hatch open / gears up & hatch closed
-// channel6 - NA
-// channel7 - SWD UP/DOWN -> cargo bay closed/open
 const byte LOGGERSWITCHCHANNEL = 4;
 const byte LANDINGGEARCHANNEL = 5;
 
-// // Threshold for discrete PPM signals
-// const int PPMTHRESHLOW = 850;
-// const int PPMTHRESHMID = 1100;
-// const int PPMTHRESHHIGH = 1350;
 
 // States for starting and stopping the logger
 bool loggerSwitchState = HIGH;
@@ -73,13 +65,13 @@ triStateSwitch landingGearSwitchStatePrev;
 
 const byte SERVOLANDINGGEAR = 0;        // the servo number of the landing gear
 const byte SERVOLANDINGHATCH = 1;   // the servo number of the landing gear hatch
-const int MAXSERVOSPEEDDEGS[NUMBEROFSERVOS] = {30, 30, 100, 100}; // Maximum speed of the servos in degrees per sec
-int servoTargetPos[NUMBEROFSERVOS] = {0, 0, 0, 0}; //The initial state of the servos
+const int MAXSERVOSPEEDDEGS[NUMBEROFSERVOS] = {30, 30}; // Maximum speed of the servos in degrees per sec
+int servoTargetPos[NUMBEROFSERVOS] = {0, 0}; //The initial state of the servos
 float servoActualPos[NUMBEROFSERVOS];
-const int SERVOLANDINGGEARPOSEXTENDED = 180;
-const int SERVOLANDINGGEARPOSRETRACTED = 0;
-const int SERVOLANDINGHATCHPOSOPEN = 180;
-const int SERVOLANDINGHATCHPOSCLOSED = 0;
+const int SERVOLANDINGGEARPOSEXTENDED = 130;
+const int SERVOLANDINGGEARPOSRETRACTED = 13;
+const int SERVOLANDINGHATCHPOSOPEN = 27;
+const int SERVOLANDINGHATCHPOSCLOSED = 176;
 
 
 
@@ -88,26 +80,23 @@ const int SERVOLANDINGHATCHPOSCLOSED = 0;
 // This block of code is only run once at the beginning
 //---------------------------------------------------------------------------------------------------------
 void setup() {
-  myLed.SetOff();
+  myLed.SetColor(MAGENTA);
   Serial.begin(9600);  // Start the serial communciation
     
   myReceiver.Init();
-
-  loggerTimer.Start();
-  servoTimer.Start();
 
   for (int i=0; i<NUMBEROFSERVOS; i++) {
     myServo[i].attach(PINSERVO[i]);
   }
 
-  // Starting pos for servos
+  // Starting pos for servos. Make sure they are in the right positin
   servoTargetPos[SERVOLANDINGGEAR] = SERVOLANDINGGEARPOSEXTENDED;
   servoTargetPos[SERVOLANDINGHATCH] = SERVOLANDINGHATCHPOSOPEN;
   for (int i = 0; i < NUMBEROFSERVOS; i++) {
     servoActualPos[i] = servoTargetPos[i];
   }
   
-
+  servoTimer.Start();
 }
 
 //---------------------------------------------------------------------------------------------------------
@@ -140,6 +129,17 @@ void loop() {
   stopLogger = (loggerSwitchState > loggerSwitchStatePrev);   //if new state is HIGH (1) and old state is LOW (1)
   loggerSwitchStatePrev = loggerSwitchState; //update the previous state for the next loop
 
+  if  (startLogger) {
+    // insert code for starting logging
+    myLed.SetColor(BLUE);
+    Serial.println("Start logging");
+  }
+  if (stopLogger) {
+    // insert code for stopping logging
+    myLed.SetColor(GREEN);
+    Serial.println("Stop logging ");
+  }
+
   // Read switch SWB (high/mid/low) for landing gear. 
   HandleLandingGearSwitch(); //Update of landingGearAction and landingGearHatchAction
 
@@ -153,9 +153,6 @@ void loop() {
   //   Serial.print("; ");
   // }
   // Serial.println();
-
-
-
 
   
   //-------------------------------------------------------------------------------------------------------
@@ -173,7 +170,7 @@ void loop() {
 //---------------------------------------------------------------------------------------------------------
 void Error(String errorMessage) {
   Serial.println(errorMessage);
-  myLed.SetBlink(100);
+  myLed.SetColor(RED);
 }
 
 
