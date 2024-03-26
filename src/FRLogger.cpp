177782a44b5ad70b99cfb8c4f0d9535bacd85455
josmeuleman <josmeuleman@gmail.com>
2024-03-26 16:49:56 +0100

    Update FRLogger.cpp

--- src/FRLogger.cpp
+++ src/FRLogger.cpp
@@ -9,6 +9,8 @@ Logger::Logger() {
   _isLogging = false;
   _fileName = "";
   _loggerString.reserve(512);
+  _headerString.clear();
+  _headerString.concat("Time[ms]; "); 
 }
 
 bool Logger::CheckSD() {
@@ -17,6 +19,7 @@ bool Logger::CheckSD() {
 
 void Logger::AddSensor(FRSensor *Sensor){
   sensors.push_back(Sensor);
+  _headerString.concat(Sensor->HeaderString());
 }
 
 String Logger::GetLoggerFileName() {
@@ -43,11 +46,10 @@ bool Logger::StartLogger() {
   }
   
   // Successfully opened a logfile. Write the header line
-  _headerString.clear();
-  _headerString.concat("Time[ms]; ");
-  for (int i = 0; i < sensors.size(); i++) {
-    _headerString.concat(sensors[i]->HeaderString());
-  }
+
+  //for (int i = 0; i < sensors.size(); i++) {
+  //  _headerString.concat(sensors[i]->HeaderString());
+  //}
   _file.println(_headerString);
 
   _isLogging = true;
