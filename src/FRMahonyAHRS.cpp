ef2c01a59123f8ca3601cb90810b26eaa177568d
josmeuleman <josmeuleman@gmail.com>
2024-04-16 20:54:15 +0200

    Update FRMahonyAHRS.cpp

--- src/FRMahonyAHRS.cpp
+++ src/FRMahonyAHRS.cpp
@@ -12,7 +12,7 @@
 // Date			Author			Notes
 // 29/09/2011	SOH Madgwick    Initial release
 // 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
-// 16/04/2024	J.H. Meuleman	Sample rate adjusted and gyro in radians/sec`
+// 16/04/2024	J.H. Meuleman	gyro in radians/sec
 //
 // Algorithm paper:
 // http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=4608934&url=http%3A%2F%2Fieeexplore.ieee.org%2Fstamp%2Fstamp.jsp%3Ftp%3D%26arnumber%3D4608934
@@ -28,7 +28,7 @@
 //-------------------------------------------------------------------------------------------
 // Definitions
 
-#define DEFAULT_SAMPLE_FREQ	10.0f	// sample frequency in Hz
+#define DEFAULT_SAMPLE_FREQ	512.0f	// sample frequency in Hz
 #define twoKpDef	(2.0f * 0.5f)	// 2 * proportional gain
 #define twoKiDef	(2.0f * 0.0f)	// 2 * integral gain
 
