a03357b0852d466a9dd866eb64df93048892c111
josmeuleman <josmeuleman@gmail.com>
2025-02-28 20:13:59 +0100

    Update FRLoggerServoIntegrationTemplate.ino

--- examples/FRLoggerServoIntegrationTemplate/FRLoggerServoIntegrationTemplate.ino
+++ examples/FRLoggerServoIntegrationTemplate/FRLoggerServoIntegrationTemplate.ino
@@ -10,26 +10,7 @@
 // - FRLibIntegration (download from https://github.com/josmeuleman/FRLibIntegration, unzipped in ../Documents/Arduino/libraries/ )
 //
 // 2024-03-21, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
-
-// Libraries from FRLibBasics
-#include <FRTimer.h>
-
-// Libraries from FRLibIntegration
-
-// Other libraries
-
-// Pins
-
-// other constants
-const int LOOPTIMESERVOMS = 10;     // Loop time for controlling servos
-const int LOOPTIMELOGGERMS = 100;   // loop time for logging the sensors
-
-// Create all objects
-Timer myServoTimer(LOOPTIMESERVOMS);// Timer object for the clock
-Timer myLoggerTimer(LOOPTIMELOGGERMS);  // Timer object for the clock
-
-// define parameters
-int nLoops = 0; // Just a counter for checking the loops
+#include "init.h"
 
 //---------------------------------------------------------------------------------------------------------
 // SETUP
@@ -48,23 +29,14 @@ void setup() {
 // This block of code is looped infinitely
 //---------------------------------------------------------------------------------------------------------
 void loop() {
-  //-------------------------------------------------------------------------------------------------------
   // Read the myReceiver and store the values
-  //-------------------------------------------------------------------------------------------------------
-
-
-  //-------------------------------------------------------------------------------------------------------
-  // Process the controller channels
-  //-------------------------------------------------------------------------------------------------------
   
-  //-------------------------------------------------------------------------------------------------------
+  // Process the controller channels
+  
   // Handle the motors
-  //-------------------------------------------------------------------------------------------------------
+  
 
-
-  //-------------------------------------------------------------------------------------------------------
   // Write to the log file if needed
-  //-------------------------------------------------------------------------------------------------------
   // Check if it is time to log the data
   if (myLoggerTimer.LoopTimePassed()) {
     // Write to the logger. This block of code will run only when the log time has passed
@@ -73,23 +45,11 @@ void loop() {
     Serial.print("; Number of times the Servoloop has run: ");
     Serial.println(nLoops);
     nLoops = 0;
-
   }
 
-  //-------------------------------------------------------------------------------------------------------
   // End of the loop
-  //-------------------------------------------------------------------------------------------------------
-  // Kill the time until
   nLoops++;
   if (myServoTimer.WaitUntilEnd()) {
     Serial.println("Overrun!");
   }
 }
-
-//---------------------------------------------------------------------------------------------------------
-// Function that prints an error to the serial port and makes the RGBLED red
-//---------------------------------------------------------------------------------------------------------
-void Error(String errorMessage) {
-  Serial.println(errorMessage);
-}
-
