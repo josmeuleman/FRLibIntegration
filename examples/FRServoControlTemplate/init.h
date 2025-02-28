c271624e741a79e06dbc8c6227600b05063a4cf7
josmeuleman <josmeuleman@gmail.com>
2025-02-28 18:08:31 +0100

    Update init.h

--- examples/FRServoControlTemplate/init.h
+++ examples/FRServoControlTemplate/init.h
@@ -8,23 +8,21 @@
 
 // Libraries from FRLibBasics
 #include <FRTimer.h>
-#include <FRButton.h>
-#include <FRRGBLED.h>
 
 // Libraries from FRLibIntegration
 #include <FRPPMReceiver.h>
 
 // Other libraries
 #include <ESP32Servo.h>
-#include "smoothservo.h"
+#include "SmoothServo.h"
 
-const byte NUMBEROFSERVOS = 2;       // Number of servos 
+const byte NUMBEROFSERVOS = 2;       // Number of Servos to control. Change if you wannt more servos
 const byte NUMBEROFCHANNELS = 8;     // Number of Channels of myReceiver
 
 // Pins
-const byte PINSERVO[NUMBEROFSERVOS] = {25, 26}; // Servo Channels
-const byte PINPPM = 4;                // PM2 input pint
-const byte PINSWITCH = 35;            // The pin number for he button to start and stop logging
+const byte PINSERVO[NUMBEROFSERVOS] = {25, 26}; // Servo pin numbers
+const byte PINPPM = 4;                // Receiver input pin
+
 
 // other constants
 const int  LOOPTIMESERVOMS = 100;     // Loop time for controlling servos. Make it 10 ms for smoother control, but then remove the Serial.print statements to prevent overruns
@@ -33,20 +31,15 @@ const int  LOOPTIMESERVOMS = 100;     // Loop time for controlling servos. Make 
 // Create all objects
 Timer myServoTimer(LOOPTIMESERVOMS);// Timer object for the clock
 SmoothServo myServo[NUMBEROFSERVOS];      // create a servo object
-RGBLED myLed;                       // Create a RGB led object. pinnummbers are defined in the library FRRGBLED.h.
 FRPPMReceiver myReceiver(PINPPM, NUMBEROFCHANNELS);  // Create a PPM receiver object with given pin and number of channels
-Button myButton(PINSWITCH, true);       // Create a button object with the given pin. True for an inverted button, false for a normal button
 
 int channelValues[NUMBEROFCHANNELS];
 const byte LOGGERSWITCHCHANNEL = 4;
-const byte LANDINGGEARCHANNEL = 5;
+const byte LANDINGGEARCHANNEL = 6;
 
 // States for starting and stopping the logger
 bool loggerSwitchState = HIGH;
 bool loggerSwitchStatePrev = HIGH;
-bool startLogger = false;
-bool stopLogger = false;
-bool servoTargetUp = true;
 
 // States for landing gear
 triStateSwitch landingGearSwitchState;
