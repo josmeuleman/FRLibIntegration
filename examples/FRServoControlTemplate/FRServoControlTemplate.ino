a68b2bd5ec1e4b6555bd213d60758ef4c7d13627
josmeuleman <josmeuleman@gmail.com>
2025-02-28 18:08:28 +0100

    Update FRServoControlTemplate.ino

--- examples/FRServoControlTemplate/FRServoControlTemplate.ino
+++ examples/FRServoControlTemplate/FRServoControlTemplate.ino
@@ -7,14 +7,14 @@
 // - 1x RGB LED 
 // Connections:
 // - TGY channel PPM, pin 1 - FR Board, PPL2 GND
-// - TGY channel PPM, pin 2 - FR Board, PPL2, D4
-// - TGY channel PPM, pin 3 - FR Board, J11 5V (or connected to other 5V source)
+// - TGY channel PPM, pin 2 - FR Board, J11 5V (or connected to other 5V source)
+// - TGY channel PPM, pin 3 - FR Board, PPL2, D4
 // Required libraries:
 // - FRLibBasics (download from https://github.com/josmeuleman/FRLibBasics, unzipped in ../Documents/Arduino/libraries/ )
 // - FRLibIntegration (download from https://github.com/josmeuleman/FRLibIntegration, unzipped in ../Documents/Arduino/libraries/ )
 // - ESP32Servo.h
 //
-// 2024-03-21, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
+// 2025-02-28, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
 
 // These are local files that contain big chunks of code to make the main document more readable. 
 // Check the corresponding files in the tab
@@ -28,7 +28,6 @@
 // This block of code is only run once at the beginning
 //---------------------------------------------------------------------------------------------------------
 void setup() {
-  myLed.SetColor(MAGENTA);
   Serial.begin(9600);  // Start the serial communciation
     
   myReceiver.Init();
@@ -36,12 +35,11 @@ void setup() {
 
   for (int i=0; i<NUMBEROFSERVOS; i++) {
     float maxStep = MAXSERVOSPEEDDEGS[i]*LOOPTIMESERVOMS/1000.0;
-    myServo[i].Init(PINSERVO[i], maxStep, SERVOSTARTPOS[i]);
+    myServo[i].Init(PINSERVO[i], maxStep, SERVOSTARTPOS[i], SERVOENDPOS[i]);
   }
 
   myServoTimer.Start();
   Serial.println("End of Setup");
-  myLed.SetColor(GREEN);  
 }
 
 //---------------------------------------------------------------------------------------------------------
@@ -49,13 +47,10 @@ void setup() {
 // This block of code is looped infinitely
 //---------------------------------------------------------------------------------------------------------
 void loop() {
-  //-------------------------------------------------------------------------------------------------------
   // Read the myReceiver and store the values
-  //-------------------------------------------------------------------------------------------------------
   for (byte i = 0; i < NUMBEROFCHANNELS; i++) {
     channelValues[i] = myReceiver.ReadChannel(i);
   }
-  
   // for debugging, print some values. This debugging slows down your program
   // for (byte i = 0; i < NUMBEROFCHANNELS; i++) {
   //   Serial.print(channelValues[i]);
@@ -63,28 +58,25 @@ void loop() {
   // }
   // Serial.println();
 
+  //-------------------------------------------------------------------------------------------------------
+  // Process the controller channels
+  //-------------------------------------------------------------------------------------------------------
+  // Read switch SWA (high/low) for logging
+  loggerSwitchState = myReceiver.IsChannelHigh(LOGGERSWITCHCHANNEL);
+  HandleLoggerSwitch(loggerSwitchState, loggerSwitchStatePrev);
+  loggerSwitchStatePrev = loggerSwitchState; //update the previous state for the next loop
 
+  // Read switch SWC (high/mid/low) for landing gear.
+  landingGearSwitchState = myReceiver.GetChannelTriState(LANDINGGEARCHANNEL);
+  // Check if landing gear and hatch need to move
+  HandleMainLandingGearSwitch(landingGearSwitchState, landingGearSwitchStatePrev, myServo[SERVOLANDINGGEAR], myServo[SERVOLANDINGHATCH]);
+  landingGearSwitchStatePrev = landingGearSwitchState; //update the previous state for the next loop
 
-  // Read switch SWB (high/mid/low) for landing gear. 
-  HandleLandingGearSwitch(); //Update of landingGearAction and landingGearHatchAction
-
-
-  myButton.Update();            // Read the state of the button
-  if (myButton.HasChangedUp()){
-    servoTargetUp = !servoTargetUp;
-    if (servoTargetUp) {
-      myServo[SERVOLANDINGGEAR].SetTargetPos(SERVOSTARTPOS[SERVOLANDINGGEAR]);
-    }
-    else {
-      myServo[SERVOLANDINGGEAR].SetTargetPos(SERVOENDPOS[SERVOLANDINGGEAR]);
-    }
-  }
-  
-  // Handle the motors. There are some debugging print statements in the Update function you want to remove to speed up the code. 
+  // Update the smooth servos. There are some debugging print statements in the Update function you want to remove to speed up the code. 
   for (int i = 0; i < NUMBEROFSERVOS; i++) {
     myServo[i].Update();
   }
-  Serial.println();
+  Serial.println(); // Remove this print statement is you remove the prints in the Servo update 
   
   // At the end of the loop, WaitUntilEnd runs until the time until looptime has passed
   if (myServoTimer.WaitUntilEnd()) {
